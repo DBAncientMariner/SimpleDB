19f5741460d2898cf16fbac944a28fca596eedaf
Parth Satra <pasatra@ncsu.edu>
2014-11-30 16:39:02 -0500

    gitignore changes
    Change-Id: Idafb0e3df7eb6c911d386e2339868093c6be6149
    Signed-off-by: Parth Satra <pasatra@ncsu.edu>

--- SimpleDBServer/src/simpledb/server/Startup.java
+++ SimpleDBServer/src/simpledb/server/Startup.java
@@ -1,5 +1,5 @@
 package simpledb.server;
-	
+
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
@@ -7,25 +7,24 @@ import simpledb.remote.RemoteDriver;
 import simpledb.remote.RemoteDriverImpl;
 
 public class Startup {
-	
+
 	private static int g_clock_value;
-	
+
 	public static int Get_clock_value() {
 		return g_clock_value;
 	}
-   
 
-public static void main(String args[]) throws Exception {
-	g_clock_value = Integer.parseInt(args[1]);  
-	// configure and initialize the database
-      SimpleDB.init(args[0]);
-      // create a registry specific for the server on the default port
-      Registry reg = LocateRegistry.createRegistry(1099);
-      
-      // and post the server entry in it
-      RemoteDriver d = new RemoteDriverImpl();
-      reg.rebind("simpledb", d);
-      
-      System.out.println("database server ready");
-   }
+	public static void main(String args[]) throws Exception {
+		g_clock_value = Integer.parseInt(args[1]);
+		// configure and initialize the database
+		SimpleDB.init(args[0]);
+		// create a registry specific for the server on the default port
+		Registry reg = LocateRegistry.createRegistry(1099);
+
+		// and post the server entry in it
+		RemoteDriver d = new RemoteDriverImpl();
+		reg.rebind("simpledb", d);
+
+		System.out.println("database server ready");
+	}
 }
